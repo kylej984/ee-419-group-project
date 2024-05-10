7cad4f6d45f0ac5a817bbb277560cce9d83fabf6
Kyle Jeng <kylej984@uw.edu>
2024-05-09 17:23:33 -0700

    Imported Project Helper

---
+++ client.py
@@ -0,0 +1,15 @@
+# Client.py
+# - Define all functions and attributes within a Client class.
+# - __init__ creates a client socket with the user entering server IP or 
+# hostname and port. After the socket is created, a connection to the server 
+# should be established. Once a connection is established between the client 
+# and server, ask the user to enter a username and send it to the server. 
+# - message_handling()Prints messages received from other users (sent via 
+# the server).
+# - input_handling() Sends user messages formatted with their username 
+# concatenated to the server.
+# - Use of threading library to allow multiple subprocesses to run 
+# simultaneously
+
+#  Comment including group member names and their contributions at the start 
+# of Client.py.
