923574317e55e802296b902fe7fc49173b72a931
Cooptheslime <63479856+Cooptheslime@users.noreply.github.com>
2024-05-20 18:58:58 -0700

    working with my dummy client sort of
    iolock is tricky

--- server.py
+++ server.py
@@ -6,31 +6,34 @@ from socket import *
 
 class Server:
     def __init__(self):
-        # clients[client] = (socket, username)
+        # clients[client] = (socket, addr, username)
         self.clients = []
-        self.serverPort = input('Server port: ')
+        serverIP = '127.0.0.1'
+        serverPort = int(input('Server port: '))
         s = socket(AF_INET, SOCK_STREAM)
-        s.bind(('127.0.0.1', serverPort))
+        s.bind((serverIP, serverPort))
         s.listen(5)
         self.serverSocket = s
         print('Server is listening on port ', serverPort)
+        self.start_server()
 
     def start_server(self):
         while True:
-            s, addr = serverSocket.accept()
+            s, addr = self.serverSocket.accept()
             print('Accepted connection from ', addr)
-            username = connectionSocket.recv(1024).decode()
-            self.clients.append((s, username))
-            new_thread = threading.Thread(target=self.client_handling, args=(self, s, addr))
+            username = s.recv(1024).decode()
+            self.clients.append((s, addr, username))
+            new_thread = threading.Thread(target=self.client_handling, args=(s, addr))
             new_thread.start()
-            # thread not stored anyway
+            # thread not stored anywhere
 
     def broadcast(self, user_message):
         for client in self.clients:
-            client.send(user_message)
+            client[0].send(user_message)
 
     def client_handling(self, client_socket, client_address):
         while True:
             data = client_socket.recv(1024)
-            
+            self.broadcast(data)
 
+server1 = Server()
