eb6fa7c06e6b830b0e832362f9245feb34b71862
Cooptheslime <63479856+Cooptheslime@users.noreply.github.com>
2024-06-02 18:44:25 -0700

    Add files via upload

--- client_coop.py
+++ client_coop.py
@@ -1,3 +1,16 @@
+# Title: Server Script
+# Authors:
+#   Cooper Wharton
+#   Kyle Jeng
+# Institution:
+#   University of Washington Seattle
+#   Spring 2024
+#   EE 419
+#   Prof. Mahmood Hameed
+# Version: 1.0
+#   Stable, Working
+# Date: 06.02.2024
+
 import random
 import string
 import time
@@ -5,6 +18,7 @@ import threading
 from socket import *
 
 
+# Client object
 class Client:
 
     def __init__(self):
@@ -19,6 +33,7 @@ class Client:
         # Input
         self.username = input("Input your username: ")
         print()
+        # 1st message to server is always your own uname
         s.send(self.username.encode())
 
     # Sends user messages formatted with their username
@@ -26,7 +41,9 @@ class Client:
     def input_handling(self):
         while True:
             message = self.username + ": " + input()
+            # black magic for making the formatting look nice
             print('\033[1A' + '\033[K', end='')
+            # Sendthe message
             self.serverSocket.send(message.encode())
 
     # Prints messages received from other users (sent via
@@ -40,5 +57,6 @@ class Client:
 client1 = Client()
 
 
+# Ideally these threads should live in the client object but storing them there causes errors
 threading.Thread(target=client1.input_handling).start()
 threading.Thread(target=client1.message_handling).start()
--- server.py
+++ server.py
@@ -1,3 +1,16 @@
+# Title: Server Script
+# Authors:
+#   Cooper Wharton
+#   Kyle Jeng
+# Institution:
+#   University of Washington Seattle
+#   Spring 2024
+#   EE 419
+#   Prof. Mahmood Hameed
+# Version: 1.0
+#   Stable, Working
+# Date: 06.02.2024
+
 import random
 import string
 import time
@@ -5,8 +18,10 @@ import threading
 from socket import *
 
 
+# Server object
 class Server:
     def __init__(self):
+        # clients contains tuples with client info as such:
         # clients[client] = (socket, addr, username)
         self.clients = []
         serverIP = '127.0.0.1'
@@ -20,16 +35,22 @@ class Server:
 
     def start_server(self):
         while True:
+            # Continuously accept connections
             s, addr = self.serverSocket.accept()
+            # Notify when connection is made
             print('Accepted connection from ', addr)
+            # first communication is always username per spec.
             username = s.recv(1024).decode()
             self.clients.append((s, addr, username))
+            # create a new anonymous thread for this client
+            # shut down of client connection is out of the scope of this project
             new_thread = threading.Thread(target=self.client_handling, args=(s, addr))
             new_thread.start()
             # thread not stored anywhere
 
     def broadcast(self, user_message):
         for client in self.clients:
+            # 1st in client tuple is socket of that connection
             client[0].send(user_message)
 
     def client_handling(self, client_socket, client_address):
